8eb66f0dbca067e68941851a8efbdac56cbc4773
hnks <hakan.karlsson-faronius@oru.se>
2024-05-14 21:30:52 +0200

    thing I missed

--- addition.pl
+++ addition.pl
@@ -1,3 +1,4 @@
-nn(mnist_net,[X, Y],Y,[0,1,2,3,4,5,6,7,8,9]) :: digit(X,Y).
+nn(mnist_net,[X, Y],Z,[0,1,2,3,4,5,6,7,8,9]) :: digit(X,Y,Z).
 
-addition(X,Y,Z) :- digit(X,X2), digit(Y,Y2), Z is X2+Y2.
+addition(X,Y,Sum) :- digit(X,Y,Z1), digit(X, Y, Z2), Sum is Z1+Z2.
+
